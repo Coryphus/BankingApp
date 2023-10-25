b46884c7b999f62366261872aadfa1d28640db69
Alex <alexander@alexburns.com>
2023-10-25 15:58:09 +0100

    Added some accounts in App

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,11 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        CurrentAccount acc1 = new CurrentAccount(10000, 1000, 1000, "Josh");
+
+        SavingsAccount acc2 = new SavingsAccount(12032, 100, 0.03, "Alex");
+
+        BankAccount acc3 = new BankAccount(400, 10, "Connor");
     }
 }
--- src/main/java/org/example/SavingsAccount.java
+++ src/main/java/org/example/SavingsAccount.java
@@ -1,9 +1,9 @@
 package org.example;
 
 public class SavingsAccount extends BankAccount{
-    float interestRate;
+    double interestRate;
 
-    public SavingsAccount(double balance, double minimumBalance, float interestRate, String accountHolderName) {
+    public SavingsAccount(double balance, double minimumBalance, double interestRate, String accountHolderName) {
         super(balance, minimumBalance, accountHolderName);
         this.interestRate = interestRate;
     }
